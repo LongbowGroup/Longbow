5a7cfc161704b7fb1657fca9de69485b4c011a32
Argo Zhang <argo@163.com>
2019-09-08 09:25:45 +0800

    refactor: 更改内部类方法为扩展方法

--- src/Longbow/InternalIPAddressExtensions.cs
+++ src/Longbow/InternalIPAddressExtensions.cs
@@ -10,7 +10,7 @@
         /// </summary>
         /// <param name="address"></param>
         /// <returns></returns>
-        public static string ToIPv4String(IPAddress address)
+        public static string ToIPv4String(this IPAddress address)
         {
             var ipv4Address = (address ?? IPAddress.IPv6Loopback).ToString();
             return ipv4Address.StartsWith("::ffff:") ? address.MapToIPv4().ToString() : ipv4Address;
